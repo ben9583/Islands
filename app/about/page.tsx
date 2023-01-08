d85490dd268283a20ab5301ee3fcf7500850a0e6
Ben Plate <bplate9583@gmail.com>
2023-01-08 15:53:18 -0700

    placeholder about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css'
+
+export default function About() {
+    return (
+        <main className={styles.main}>
+            <div>
+                <h1>About</h1>
+                <p>This is a placeholder page! Information will be added here when the app is further along in development.</p>
+            </div>
+        </main>
+    )
+}
