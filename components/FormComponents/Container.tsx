61c3ab2c0fbb1c098887c58408b96062dcf2ee62
Ben Plate <bplate9583@gmail.com>
2023-01-16 15:13:33 -0800

    update container definition

--- components/FormComponents/Container.tsx
+++ components/FormComponents/Container.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 
-export default function Container({ children, height = 300 }) {
+type ContainerProps = {
+  children: React.ReactNode,
+  height: number,
+}
+
+export default function Container(props: ContainerProps) {
+  const { children, height } = props
   return (
     <div style={{width: 400, padding: 25, height, border: "1px solid #a746a7", borderRadius: 10, display: "flex", flexDirection: "column", position: "relative"}}>
       {children}
