93499958f76b294919872c2ce3d5a20c2be199f1
Ben Plate <bplate9583@gmail.com>
2023-01-22 13:49:01 -0800

    create login/logout guards

---
+++ components/LogoutGuard.tsx
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import { getServerSideUser, UserStatus } from "../lib/user"
+
+// Normally you can't use async functions to return a React Component,
+// but this is a server component so it's OK. 
+// Typescript doesn't know that however, so we have this workaround.
+interface PromiseJSXElement extends JSX.Element {}
+
+export default async function LogoutGuard({
+  children,
+}: {
+  children: React.ReactNode;
+  // @ts-ignore
+}): PromiseJSXElement {
+  const user = await getServerSideUser(cookies())
+
+  if(user.status === UserStatus.Registered) {
+    redirect("/app")
+  } else if(user.status === UserStatus.NotRegistered) {
+    redirect("/confirm-registration")
+  }
+
+  return (
+    <>{children}</>
+  )
+}
