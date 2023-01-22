58b514a4437f664884b195b0bf29acc3c02808ad
Ben Plate <bplate9583@gmail.com>
2023-01-22 13:49:31 -0800

    use guards where applicable

--- app/(landing)/login/page.tsx
+++ app/(landing)/login/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import LogoutGuard from "../../../components/LogoutGuard"
 import Container from "../../../components/FormComponents/Container"
 import Separator from "../../../components/FormComponents/Separator"
 import Integration from "../../../components/Integration"
@@ -6,22 +7,24 @@ import styles from "./page.module.css"
 
 export default function Login() {
   return (
-    <main className={styles.main}>
-      <Container height={300}>
-        <div>
-          <h1>Login</h1>
-        </div>
-        <div>
-          <Separator />
-        </div>
-        <div style={{ margin: "10px 0" }}>
-          <p>Choose one of the following integrations below to continue.</p>
-        </div>
-        <div className={styles.integrations}>
-          <Integration provider="discord" width="60%" height={40} />
-          <Integration provider="github" width="60%" height={40} />
-        </div>
-      </Container>
-    </main>
+    <LogoutGuard>
+      <main className={styles.main}>
+        <Container height={300}>
+          <div>
+            <h1>Login</h1>
+          </div>
+          <div>
+            <Separator />
+          </div>
+          <div style={{ margin: "10px 0" }}>
+            <p>Choose one of the following integrations below to continue.</p>
+          </div>
+          <div className={styles.integrations}>
+            <Integration provider="discord" width="60%" height={40} />
+            <Integration provider="github" width="60%" height={40} />
+          </div>
+        </Container>
+      </main>
+    </LogoutGuard>
   )
 }
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,12 @@
+import LoginGuard from "../../components/LoginGuard"
 import styles from "./page.module.css"
 
 export default function Login() {
   return (
-    <main>
-      <h1>hi</h1>
-    </main>
+    <LoginGuard>
+      <main>
+        <h1>hi</h1>
+      </main>
+    </LoginGuard>
   )
 }
