a381b077ef3c042d2966ee3dd68c0f20260da903
Ben Plate <bplate9583@gmail.com>
2023-01-22 13:48:44 -0800

    fix messages for confirm registration

--- components/FormComponents/InputFields.tsx
+++ components/FormComponents/InputFields.tsx
@@ -5,54 +5,137 @@ import styles from "./InputFields.module.css"
 
 export default function InputFields() {
   let [ username, setUsername ] = useState("")
-  let [ message, setMessage ] = useState("")
-  let [ usernameValid, setUsernameValid ] = useState(false)
+  let [ usernameMessage, setUsernameMessage ] = useState<string | undefined>(undefined)
+  let [ firstName, setFirstName ] = useState("")
+  let [ firstNameMessage, setFirstNameMessage ] = useState<string | undefined>(undefined)
+  let [ lastName, setLastName ] = useState("")
+  let [ lastNameMessage, setLastNameMessage ] = useState<string | undefined>(undefined)
+  let [ displayName, setDisplayName ] = useState("")
+  let [ displayNameMessage, setDisplayNameMessage ] = useState<string | undefined>(undefined)
 
-  useEffect(() => {
-    setUsernameValid(false)
+  let [ createUserMessage, setCreateUserMessage ] = useState<string | undefined>(undefined)
 
-    if(username.length > 0 && username.length < 3) {
-      setMessage("Username must be at least 3 characters long.")
+  const checkUsername = () => {
+    setCreateUserMessage(undefined)
+
+    if(username.length > 0 && (username.length < 3 || username.length > 16)) {
+      setUsernameMessage("Username must be between 3 and 16 characters long.")
       return
     } else if(username.length === 0) {
-      setMessage("")
+      setUsernameMessage("")
       return
     }
 
     if(!username.match(/^[a-zA-Z0-9]*$/)) {
-      setMessage("Username must only contain alphanumeric characters.")
+      setUsernameMessage("Username must only contain alphanumeric characters.")
       return
     }
 
-    fetch(window.location.origin + "/api/user/" + username, {
+    fetch(window.location.origin + "/api/user/get/" + username, {
       headers: {
         "Content-Type": "application/json",
       },
     }).then((res) => {
       if(res.status !== 404) {
-        setMessage("Username is not available.")
+        setUsernameMessage("Username is not available.")
       } else {
-        setMessage("")
+        setUsernameMessage(undefined)
       }
     })
+  }
 
-    setUsernameValid(true)
-  }, [username])
+  const checkFirstName = () => {
+    setCreateUserMessage(undefined)
+    setFirstNameMessage("")
+
+    if(firstName.length > 0 && firstName.length > 32) {
+      setFirstNameMessage("First name must be between 1 and 32 characters long.")
+      return
+    } else if(firstName.length === 0) {
+      setFirstNameMessage("")
+      return
+    }
+
+    setFirstNameMessage(undefined)
+  }
+
+  const checkLastName = () => {
+    setCreateUserMessage(undefined)
+    setLastNameMessage("")
+
+    if(lastName.length > 0 && lastName.length > 32) {
+      setLastNameMessage("Last name must be between 1 and 32 characters long.")
+      return
+    } else if(lastName.length === 0) {
+      setLastNameMessage("")
+      return
+    }
+
+    setLastNameMessage(undefined)
+  }
+
+  const checkDisplayName = () => {
+    setCreateUserMessage(undefined)
+    setDisplayNameMessage("")
+
+    if(displayName.length > 0 && displayName.length > 32) {
+      setDisplayNameMessage("Display name must be between 1 and 32 characters long.")
+      return
+    } else if(displayName.length === 0) {
+      setDisplayNameMessage("")
+      return
+    }
+
+    setDisplayNameMessage(undefined)
+  }
+
+  useEffect(checkUsername, [username])
+  useEffect(checkFirstName, [firstName])
+  useEffect(checkLastName, [lastName])
+  useEffect(checkDisplayName, [displayName])
 
   return (
     <>
       <div>
-        <input type="text" className={styles.input} placeholder="First Name" style={{width: "47.5%", marginRight: "2.5%"}} />
-        <input type="text" className={styles.input} placeholder="Last Name" style={{width: "47.5%", marginLeft: "2.5%"}} />
+        <input type="text" className={styles.input} placeholder="First Name" style={{width: "47.5%", marginRight: "2.5%"}} onChange={(input) => setFirstName(input.currentTarget.value)} />
+        <input type="text" className={styles.input} placeholder="Last Name" style={{width: "47.5%", marginLeft: "2.5%"}} onChange={(input) => setLastName(input.currentTarget.value)} />
       </div>
       <div>
         <input type="text" className={styles.input} placeholder="Username" style={{width: "100%"}} onChange={(input) => setUsername(input.currentTarget.value)} />
       </div>
       <div>
-        <input type="text" className={styles.input} placeholder="Display Name" style={{width: "100%"}} />
+        <input type="text" className={styles.input} placeholder="Display Name" style={{width: "100%"}} onChange={(input) => setDisplayName(input.currentTarget.value)} />
       </div>
       <div>
-        <p style={{color: "#f00"}}>{message}</p>
+        <button className={styles.inputSubmitButton} style={{width: "100%"}} disabled={firstNameMessage !== undefined && lastNameMessage !== undefined && usernameMessage !== undefined && displayNameMessage !== undefined} onClick={() => {
+          if(firstNameMessage !== undefined || lastNameMessage !== undefined || usernameMessage !== undefined || displayNameMessage !== undefined) return
+
+          fetch(window.location.origin + "/api/user/create", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: username,
+              first_name: firstName,
+              last_name: lastName,
+              display_name: displayName,
+            }),
+          }).then((res) => {
+            if(res.status === 201) {
+              window.location.href = "/app"
+            } else {
+              res.json().then((json) => {
+                setCreateUserMessage(json.error)
+              }).catch(() => {
+                setCreateUserMessage(`An error occured: ${res.status} ${res.statusText ?? ""}`)
+              })
+            }
+          })
+        }}>Next</button>
+      </div>
+      <div>
+        <p style={{color: "#f00"}}>{firstNameMessage ?? lastNameMessage ?? usernameMessage ?? displayNameMessage ?? createUserMessage ?? ""}</p>
       </div>
     </>
   )
